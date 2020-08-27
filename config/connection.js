bfee6d3f4ccfb2a7518b13f422db0176ead73c42
Jonghyun Kang <misterjaykay@gmail.com>
2020-08-26 21:57:12 -0400

    server.js and orm.js codes has been written

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
-
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "password",
   database: "burgers_db"
 });
 
--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,41 @@
 var connection = require("./config/connection");
 
-var orm = 
-selectAll()
-insertOne()
-updateOne()
+var orm = {
+    // SELECT * FROM burgers
+    selectAll: function(tableInput, cb) {
+        var qString = "SELECT * FROM ??";
+        connection.query(qString, tableInput, function(err, result){
+            if (err) { throw err; }
+            cb(result);
+        })
+    },
+
+    // INSERT INTO burgers(burger_name, devoured)
+    // VALUES ("Cheeseburger", false)
+    insertOne: function(tableInput, colInput, colVal, cb) {
+        var qString = `INSERT INTO ${tableInput}`;
+        qString += `(${colInput}) `;
+        qString += `VALUES (${colVal});`
+        connection.query(qString, function(err, result){
+            if (err) { throw err; }
+            cb(result);
+        })
+    },
+
+    // UPDATE burgers
+    // SET devoured = true
+    // WHERE CustomerID = 1;
+    updateOne: function(tableInput, colInput, colVal, id, cb) {
+        var qString = `UPDATE ${tableInput} `;
+        qString += `SET ${colInput} = ${colVal} `;
+        qString += `WHERE id=?`;
+        connection.query(qString, id, function(err,result){
+            if (err) { throw err; }
+            cb(result);
+        })
+    }
+}
+
 
 
 module.exports = orm;
